598b9009107488c4e1d16b418d3d293d638bea27
tomhste <tomhste@gmail.com>
2016-02-09 16:17:32 +0000

    sum changes
    sum changes

--- main.cpp
+++ main.cpp
@@ -1,131 +1,25 @@
 #include <iostream>
+#include "main.h"
 
 using namespace std;
 
-class IndexOutOfRangeException{};
-
-struct IntNode
-{
-public:
-    int data;
-    IntNode* next;
-
-    IntNode(int Data, IntNode* Next = NULL)
-    {
-        data = Data;
-        next = Next;
-    }
-};
-
-typedef IntNode* NodePtr;
-
-class IntList
-{
-private:
-    NodePtr head;
-    NodePtr tail;
-    int count;
-
-public:
-    //constant
-    IntList()
-    {
-        head = NULL;
-        tail = NULL;
-        count = 0;
-    }
-    ~IntList()
-    {
-        NodePtr temp = head;
-        while(temp != NULL)
-        {
-            head = head->next;
-            delete temp;
-            temp = head;
-        }
-        tail = NULL;
-        count = 0;
-    }
-    //constant
-    void append(int elem)
-    {
-        NodePtr node = new IntNode(elem);
-        if(count == 0)
-        {
-            head = node;
-            tail = node;
-        }
-        else
-        {
-            tail->next = node;
-            tail = node;
-        }
-        count++;
-    }
-    //linulegt
-    void remove(int index)
-    {
-        if(0 <= index && index < count)
-        {
-            if(index == 0)
-            {
-                NodePtr temp = head;
-                head = head->next;
-                delete temp;
-            }
-            else
-            {
-                NodePtr temp = head;
-                for(int i = 1; i < index; i++)
-                {
-                    temp = temp->next;
-                }
-                NodePtr target = temp->next;
-                temp->next = temp->next->next;
-                delete target;
-            }
-            count--;
-            return;
-        }
-        throw IndexOutOfRangeException();
-    }
-
-    friend ostream& operator<< (ostream &out, IntList &list);
-};
-
-ostream& operator<< (ostream &out, IntList &list)
-{
-    NodePtr temp = list.head;
-    for(int i = 0; i < list.count; i++)
-    {
-        out << temp->data << " ";
-        temp = temp->next;
-    }
-    return out;
-}
-
 int main()
 {
-    IntList list;
-
+    Counter counter;
     while(true)
     {
         string cmd;
         cin >> cmd;
-        if(cmd == "append")
+        if(cmd == "add")
         {
-            int elem;
-            cin >> elem;
-            list.append(elem);
+            string word;
+            cin >> word;
+            counter.incr(word);
         }
-        if(cmd == "remove")
+        if(cmd == "print")
         {
-            int index;
-            cin >> index;
-            list.remove(index);
+            counter.get_by_freq();
         }
-        cout << list << endl;
     }
-
     return 0;
 }
---
+++ main.h
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+struct OurDataType
+{
+public:
+    int frequency;
+    string word;
+    OurDataType(string word)
+    {
+        this->word = word;
+        frequency = 1;
+    }
+};
+
+
+bool alphabetical(string a, string b)
+{
+    int x = min(a.length(), b.length());
+
+    for(int i = 0; i < x; i++)
+    {
+        if(a[i] > b[i])
+            return false;
+    }
+
+    return true;
+}
+
+bool operator < (OurDataType a, OurDataType b)
+{
+    if(a.frequency == b.frequency)
+    {
+        return alphabetical(a.word, b.word);
+    }
+    else
+    {
+        return b.frequency < a.frequency;
+    }
+}
+
+class Counter
+{
+private:
+    vector<OurDataType> words;
+public:
+    //linulegt
+    void incr(string word)
+    {
+        for(unsigned int i = 0; i < words.size(); i++)
+        {
+            if(words[i].word == word)
+            {
+                words[i].frequency++;
+                return;
+            }
+        }
+        //We didn't find the word in the vector so we create it
+        OurDataType w(word);
+        words.push_back(w);
+    }
+    //linulegt
+    void get_by_freq()
+    {
+        //We can do this so easily because we made a custom < operator for OurDataType
+        sort(words.begin(), words.end());
+        for(unsigned int i = 0; i < words.size(); i++)
+        {
+            cout << words[i].word << " " << words[i].frequency << endl;
+        }
+    }
+};
